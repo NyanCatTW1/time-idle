39ff21056d98eef405110faa3b3b0c4e3a8e4927
e666666 <17372086+e666666@users.noreply.github.com>
2020-07-25 21:57:11 +0800

    Nerf cash gain after tick 100

--- js/layers/rebuild/cash.js
+++ js/layers/rebuild/cash.js
@@ -2,7 +2,7 @@ function getCashAvailable() {
   if (player.tickEver.lt(100)) return new Decimal(0)
   
   let ret = new Decimal(100)
-  ret = ret.plus(player.tickEver.minus(100).times(10))
+  ret = ret.plus(player.tickEver.minus(100).times(4))
 
   for (let i = 1; i < problemGoals.length; i++) {
     ret = ret.plus(getProblemReward(i))
